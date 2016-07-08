00586b088e2e1db73e830e8d90b26e6b3a0ca6b9
Tony9866 <zz7057709423@outlook.com>
2016-07-08 19:13:49 -0400

    lab4

---
+++ COMP2007-LAB4/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COMP2007_LAB4.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET: /Home/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //GET: /Home/About
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
+        //GET: /Home/Contact
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
